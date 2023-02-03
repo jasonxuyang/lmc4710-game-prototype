6753a315d6a51ee50fd4c8fa4ea75b81165792ba
A-Lindquist <70296335+A-Lindquist@users.noreply.github.com>
2023-02-03 17:28:09 -0500

    Updated PlayerMovement Script
    - PlayerMovement -> PlayerController
    - moved player movement into a separate method
    - added Scripts folder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed = 50.0f;
     public float rate = 3f;
 
-    // Update is called once per frame
+    // Update Player 
     void Update()
+    {
+        GetMovement();
+    }
+
+    // Update Player Movement
+    private void GetMovement()
     {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
@@ -18,24 +24,17 @@ public class PlayerMovement : MonoBehaviour
         //Vector2 movement = new Vector2(speed * inputX, speed * inputY);
         //movement *= Time.deltaTime;
 
-        
         // Uniform Movement
-        float x;
-        float y;
         if (inputX != 0 && inputY != 0)
         {
             float mag = (float)(Math.Sqrt(Math.Pow(inputX, 2)) * Math.Sqrt(Math.Pow(inputY, 2)));
             float angle = (float)(Math.Acos(0 / mag) / 2);
 
-            x = (float)(Math.Cos(angle) * inputX);
-            y = (float)(Math.Sin(angle) * inputY);
-        }
-        else
-        {
-            x = inputX; y = inputY;
+            inputX *= (float)Math.Cos(angle);
+            inputY *= (float)Math.Sin(angle);
         }
 
-        Vector2 movement = new Vector2(x * speed, y * speed);
+        Vector2 movement = new Vector2(inputX * speed, inputY * speed);
         movement *= Time.deltaTime / rate;
 
         transform.Translate(movement);
