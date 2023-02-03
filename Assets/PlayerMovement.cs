f8f4710677446d634bd65e45896e1e104e3236a5
A-Lindquist <70296335+A-Lindquist@users.noreply.github.com>
2023-02-03 01:15:59 -0500

    Implemented Uniform player movement
    Commented out previous movement
    Added uniform movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Vector2 speed = new Vector2(50, 50);
+    public float speed = 50.0f;
+    public float rate = 3f;
 
     // Update is called once per frame
     void Update()
@@ -12,9 +14,29 @@ public class PlayerMovement : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+        //ORIGINAL SCRIPT FOR MOVEMENT
+        //Vector2 movement = new Vector2(speed * inputX, speed * inputY);
+        //movement *= Time.deltaTime;
 
-        movement *= Time.deltaTime;
+        
+        // Uniform Movement
+        float x;
+        float y;
+        if (inputX != 0 && inputY != 0)
+        {
+            float mag = (float)(Math.Sqrt(Math.Pow(inputX, 2)) * Math.Sqrt(Math.Pow(inputY, 2)));
+            float angle = (float)(Math.Acos(0 / mag) / 2);
+
+            x = (float)(Math.Cos(angle) * inputX);
+            y = (float)(Math.Sin(angle) * inputY);
+        }
+        else
+        {
+            x = inputX; y = inputY;
+        }
+
+        Vector2 movement = new Vector2(x * speed, y * speed);
+        movement *= Time.deltaTime / rate;
 
         transform.Translate(movement);
     }
